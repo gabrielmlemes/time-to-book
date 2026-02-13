e5a731aefc3f3e9147cb7269b699c0abc0b8374e
Gabriel Lemes <g.moreiralemess@gmail.com>
2026-02-13 15:44:31 -0300

    chore: add ESLint, Prettier, and Commitlint configurations with Husky support

---
+++ commitlint.config.js
@@ -0,0 +1,7 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [0, 'always'],
+  },
+};
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,10 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
+import pluginReactHooks from "eslint-plugin-react-hooks";
+import pluginPrettier from "eslint-plugin-prettier";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -9,8 +13,29 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+export default [
+  // Configurações padrão do Next.js
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
 
-export default eslintConfig;
+  // Regras personalizadas
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    plugins: {
+      "react-hooks": pluginReactHooks,
+      prettier: pluginPrettier,
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "prettier/prettier": "error",
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+];
