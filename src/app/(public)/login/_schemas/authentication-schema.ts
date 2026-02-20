91cabbb1efa4ae99cfde7804eb18532c16917cd2
Gabriel Lemes <g.moreiralemess@gmail.com>
2026-02-20 09:07:12 -0300

    feat: implement initial authentication page with sign-in and sign-up forms

---
+++ src/app/(public)/login/_schemas/authentication-schema.ts
@@ -0,0 +1,15 @@
+import z from 'zod';
+
+export const signupSchema = z.object({
+  name: z.string().trim().min(3, { message: 'O nome é obrigatório' }).max(20),
+  email: z.email({ message: 'Insira um email válido' }).trim(),
+  password: z.string().trim().min(8, { message: 'A senha deve ter no mínimo 8 caracteres' }),
+  //   confirmPassword: z.string().min(8),
+});
+export type SignupSchema = z.infer<typeof signupSchema>;
+
+export const signInSchema = z.object({
+  email: z.email({ message: 'Insira um email válido' }).trim(),
+  password: z.string().trim().min(8, { message: 'A senha deve ter no mínimo 8 caracteres' }),
+});
+export type SignInSchema = z.infer<typeof signInSchema>;
---
+++ src/app/(public)/login/page.tsx
@@ -0,0 +1,27 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+
+import { SignInForm } from './_components/sign-in-form';
+import { SignUpForm } from './_components/sign-up-form';
+
+const AuthenticationPage = () => {
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+      <Tabs defaultValue="login" className="w-[400px]">
+        <TabsList className="w-full">
+          <TabsTrigger value="login">Entrar</TabsTrigger>
+          <TabsTrigger value="signup">Criar conta</TabsTrigger>
+        </TabsList>
+
+        <TabsContent value="login">
+          <SignInForm />
+        </TabsContent>
+
+        <TabsContent value="signup">
+          <SignUpForm />
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+};
+
+export default AuthenticationPage;
